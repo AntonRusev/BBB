5aa658f676b8db2015b2ac961b1c3d94e157c340
AntonRusev <anton_r@abv.bg>
2026-03-15 22:22:53 +0200

    added button to clear recently viewed products

--- components/products/RecentlyViewed.tsx
+++ components/products/RecentlyViewed.tsx
@@ -5,6 +5,9 @@ import ProductCard from "./ProductCard"
 
 export default function RecentlyViewed() {
     const products = useRecentlyViewedStore((s) => s.products)
+    const clearRecentlyViewed = useRecentlyViewedStore(
+        (s) => s.clearRecentlyViewed
+    )
 
     // If there are no recently viewed products show nothing
     if (products.length === 0) return null;
@@ -15,6 +18,14 @@ export default function RecentlyViewed() {
             <h2 className="text-xl font-semibold mb-4">
                 Recently Viewed
             </h2>
+
+            {/* Clear recently viewed items button */}
+            <button
+                onClick={clearRecentlyViewed}
+                className="text-sm font-medium my-2 text-red-600 hover:text-red-400 transition-colors hover:cursor-pointer"
+            >
+                Clear Recently Viewed
+            </button>
 
             {/* THe number of columns in the grid should be equal to the number of products tracked/shown (the value of the MAX_ITEMS variable in the RecentlyViewed store ) */}
             <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4">
